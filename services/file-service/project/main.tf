09242a1466f0600c3c5dac3079f9ffcec581e979
IvanKalyta2025 <ivan25sfj@gmail.com>
2026-01-20 16:10:13 +0100

    Add: Terraform state files for YOKO-markert repository and collaborators
    - Created terraform.tfstate with resource definitions for the GitHub repository and collaborators.
    - Added terraform.tfstate.backup as a backup of the previous state.
    - Configured repository settings including permissions for collaborators.

--- services/file-service/project/main.tf
+++ services/file-service/project/main.tf
@@ -9,3 +9,35 @@ required_providers {
     }
 }
 }
+variable "github_token" {
+    sensitive = true
+}
+
+provider "github" {
+    token = var.github_token
+}
+
+resource "github_repository" "remote" {
+    name       = "YOKO-markert"
+    visibility = "public"
+}
+
+output "remote_repo" {
+    value = github_repository.remote.http_clone_url
+}
+
+locals {
+    workers = [
+    "IvanKalytaYOKO",
+    "jespergran98",
+    ]
+}
+
+resource "github_repository_collaborator" "admins" {
+    # for_each is HashiCorp Configuration Language's (HCL) "hacky" variation of a loop
+    for_each = toset(local.workers)
+
+    repository = github_repository.remote.name
+    username   = each.value
+    permission = "admin"
+    }
