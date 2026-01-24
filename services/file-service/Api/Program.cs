6a86b18f0311294e1a77ded571837630bf184d06
IvanKalyta2025 <ivan25sfj@gmail.com>
2026-01-24 16:18:10 +0100

    Add: Configure database and server settings, implement user registration form with styling

--- services/file-service/Api/Program.cs
+++ services/file-service/Api/Program.cs
@@ -9,13 +9,15 @@ using System.Security.Claims;
 using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 var builder = WebApplication.CreateBuilder(args);
-// var appswagger = builder.Build();
 
+// --- Настройка БД ---
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql("Host=postgres;Database=filesdb;Username=user;Password=pass"));
 
+// --- Настройка Порта (важно для Traefik) ---
 builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
+// --- Сервисы ---
 builder.Services.AddScoped<IUserRepository, UserRepositoryDb>();
 builder.Services.AddScoped<IProfileRepository, ProfileRepository>();
 builder.Services.AddEndpointsApiExplorer();
@@ -25,17 +27,21 @@ builder.Services.AddControllers();
 builder.Services.AddScoped<FileService>();
 
 var app = builder.Build();
+
+// 1. Прокси заголовки (Всегда первым для Traefik)
 app.UseForwardedHeaders(new ForwardedHeadersOptions
 {
     ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
 });
 
+// 2. Swagger (только в Dev)
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// 3. Миграции
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
@@ -46,19 +52,20 @@ using (var scope = app.Services.CreateScope())
     }
     catch (Exception ex)
     {
-        // Если база еще не готова, здесь можно логировать ошибку
         Console.WriteLine($"Ошибка при миграции: {ex.Message}");
     }
 }
 
+// --- ВАЖНО: Статические файлы СНАЧАЛА ---
 
-app.UseAuthorization();
-app.MapControllers();
+// Превращает запрос "/" в "/index.html"
+app.UseDefaultFiles();
+
+// Отдает файлы из папки wwwroot
 app.UseStaticFiles();
 
+// --- Логика API ПОТОМ ---
+app.UseAuthorization();
+app.MapControllers();
+
 app.Run();
-
-
-
-// builder.Services.AddDbContext<AppDbContext>(options =>
-//     options.UseNpgsql("Host=postgres;Database=filesdb;Username=user;Password=pass"));
