7c130e27690006d458ed73374ac529dc64f4d1a1
IvanKalyta2025 <ivan25sfj@gmail.com>
2026-01-25 16:29:52 +0100

    Add: change password section and UI toggle logic

--- services/file-service/Api/Migrations/20260125142747_InitialCreate.Designer.cs
+++ services/file-service/Api/Migrations/20260125142747_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260125142015_InitialCreate")]
+    [Migration("20260125142747_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
