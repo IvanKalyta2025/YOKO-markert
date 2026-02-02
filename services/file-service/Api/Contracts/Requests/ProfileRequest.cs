f0a585dd042ef0b2bf60dc5b6b54a765b610727c
IvanKalyta2025 <ivan25sfj@gmail.com>
2026-02-02 13:05:53 +0100

    Add: Gender enum to Domain entities

--- services/file-service/Api/Contracts/Requests/ProfileRequest.cs
+++ services/file-service/Api/Contracts/Requests/ProfileRequest.cs
@@ -1,13 +1,14 @@
 using Microsoft.AspNetCore.Http;
 using System.ComponentModel.DataAnnotations;
+using Api.Domain.Entities;
+
+
 namespace Api.Contracts.Requests
 {
     public class ProfileRequest
     {
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
-
-        [Range(16, 99, ErrorMessage = "Alder må være mellom 16 og 99 år")] //using System.ComponentModel.DataAnnotations;
         public int Age { get; set; } = 0; //update to version 2.1
         public Gender Gender { get; set; } = Gender.Other; //update to version 2.1.1 "from gender variable"
         public string Hobby { get; set; } = string.Empty; //update to version 2.1
@@ -15,10 +16,4 @@ namespace Api.Contracts.Requests
         public IFormFile? AvatarFile { get; set; } = null!;
     }
 
-    public enum Gender
-    {
-        Male,
-        Female,
-        Other
-    }
 }
--- services/file-service/Api/Domain/Entities/Profile.cs
+++ services/file-service/Api/Domain/Entities/Profile.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Api.Contracts.Requests;
 
 namespace Api.Domain.Entities
 {
@@ -10,8 +11,10 @@ namespace Api.Domain.Entities
         public Guid UserId { get; set; }
         public string LastName { get; set; } = string.Empty;
         public string FirstName { get; set; } = string.Empty;
-        public int Age { get; set; } //update to version 2.1
-        public string Gender { get; set; } = string.Empty; //update to version 2.1
+
+        [Range(16, 99, ErrorMessage = "Alder må være mellom 16 og 99 år")] //using System.ComponentModel.DataAnnotations;
+        public int Age { get; set; } = 0; //update to version 2.1
+        public Gender Gender { get; set; } = Gender.Other; //update to version 2.1
         public string Hobby { get; set; } = string.Empty; //update to version 2.1
         public string MyPlaceOfBirth { get; set; } = string.Empty; //update to version 2.1
         public string AvatarUrl { get; set; } = string.Empty;
@@ -19,5 +22,11 @@ namespace Api.Domain.Entities
         [ForeignKey(nameof(UserId))]
         public User User { get; set; } = null!;
 
+    }
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
     }
 }
